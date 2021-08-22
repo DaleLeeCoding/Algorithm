0ac3d57d9659a2a5b4f230894e3bc7eed6d5fe3e
SEUNGWOO LEE <seungwoodalelee@gmail.com>
2021-08-23 00:16:06 +0900

    try1

--- Baekjoon/1912.py
+++ Baekjoon/1912.py
@@ -1,6 +1,7 @@
 import sys
-sys.stdin = open("./input.txt", "r")
+sys.stdin = open("G:/내 드라이브/algorithm/Algorithm/Baekjoon/input.txt", "r")
 
+#https://www.acmicpc.net/problem/1912
 n = int(input())
 numbers = list(map(int, input().split()))
 print('n:', n)
