445e2f08fe221b20f3de7a2cacdd5ed80bb13eec
DaleLeeCoding <kiwilee2@korea.ac.kr>
2021-11-24 18:28:14 +0900

    1124

--- Baekjoon/3046.py
+++ Baekjoon/3046.py
@@ -1,6 +1,5 @@
 #https://www.acmicpc.net/problem/3046
 import sys
 sys.stdin = open("./input.txt", "r")
-r1, s = map(int, sys.stdin.readline().strip().split())
-print(2*s-r1)
-
+n, m = map(int, sys.stdin.readline().strip().split())
+print(n*m-1)
